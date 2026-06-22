815c9c9998214f4d7e5479a68c0b38ef570dc055
Luisito Programa <devluisitoprograma@gmail.com>
2026-06-22 00:18:01 -0500

    upgrade

--- js/pos-main.js
+++ js/pos-main.js
@@ -1,42 +1,42 @@
 (function() {
-  "use strict";
+"use strict";
 
-  /**
-   * Toggle mobile nav dropdowns
-   */
-  document.querySelectorAll('.navmenu .toggle-dropdown').forEach(navmenu => {
-    navmenu.addEventListener('click', function(e) {
-      e.preventDefault();
-      this.parentNode.classList.toggle('active');
-      this.parentNode.nextElementSibling.classList.toggle('dropdown-active');
-      e.stopImmediatePropagation();
-    });
-  });
+/**
+ * Toggle mobile nav dropdowns
+ */
+document.querySelectorAll('.navmenu .toggle-dropdown').forEach(navmenu => {
+navmenu.addEventListener('click', function(e) {
+e.preventDefault();
+this.parentNode.classList.toggle('active');
+this.parentNode.nextElementSibling.classList.toggle('dropdown-active');
+e.stopImmediatePropagation();
+});
+});
 
-  /**
-   * Navmenu Scrollspy
-   */
-  let navmenulinks = document.querySelectorAll('.navmenu a');
+/**
+* Navmenu Scrollspy
+*/
+let navmenulinks = document.querySelectorAll('.navmenu a');
 
-  function navmenuScrollspy() {
-    navmenulinks.forEach(navmenulink => {
-      if (!navmenulink.hash) return;
-      let section = document.querySelector(navmenulink.hash);
-      if (!section) return;
-      
-      // Obtener la posición del viewport del elemento
-    let rect = section.getBoundingClientRect();
-    let position = window.scrollY + 200;
-    
-      if (rect.top <= 200 && rect.bottom >= 200) {
-        document.querySelectorAll('.navmenu a.active').forEach(link => link.classList.remove('active'));
-        navmenulink.classList.add('active');
-      } else {
-        navmenulink.classList.remove('active');
-      }
-    })
-  }
-  window.addEventListener('load', navmenuScrollspy);
-  document.addEventListener('scroll', navmenuScrollspy);
+function navmenuScrollspy() {
+navmenulinks.forEach(navmenulink => {
+if (!navmenulink.hash) return;
+let section = document.querySelector(navmenulink.hash);
+if (!section) return;
+
+// Obtener la posición del viewport del elemento
+let rect = section.getBoundingClientRect();
+let position = window.scrollY + 200;
+
+if (rect.top <= 200 && rect.bottom >= 200) {
+document.querySelectorAll('.navmenu a.active').forEach(link => link.classList.remove('active'));
+navmenulink.classList.add('active');
+} else {
+navmenulink.classList.remove('active');
+}
+})
+}
+window.addEventListener('load', navmenuScrollspy);
+document.addEventListener('scroll', navmenuScrollspy);
 
 })();
